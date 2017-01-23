ea98cbccdd694e0918fab00d3cf170b2b8740692
ToddRizley <todd.rizley@gmail.com>
2017-01-23 16:39:43 -0500

    add rspec tests for movie model

--- spec/factories/movie.rb
+++ spec/factories/movie.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :movie do |f|
-    # f.association :user, factory: 'user'
+    # f.association :oscar_category, factory: 'oscar_category'
     f.title 'Test Movie 1'
     f.status 'Not Seen'
     f.oscar_year '2017'
---
+++ spec/factories/oscar_category.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :oscar_category do |f|
+    # f.association :movies, factory: 'movie'
+    f.name 'Best Picture'
+  end
+end
