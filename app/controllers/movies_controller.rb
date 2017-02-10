1e74ad573abe4e7e5cd96ddc9761171df62f0c6c
ToddRizley <todd.rizley@gmail.com>
2017-02-10 00:18:31 -0500

    update scss on movie page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,20 @@
 class MoviesController < ApplicationController
-   def show
-     movie_id = params['id'].to_i
-     @movie = Movie.includes(:oscar_categories, :theaters).find(movie_id)
-   end
+  def show
+    movie_id = params['id'].to_i
+    @movie = Movie.includes(:oscar_categories, :theaters).find(movie_id)
+  end
 
-   def update
-     movie_id = params['movie_id'].to_i
-     movie = Movie.find(movie_id)
-     movie.update_status
+  def update
+    movie_id = params['movie_id'].to_i
+    movie = Movie.find(movie_id)
+    movie.update_status
 
-     redirect_to movies_path
-   end
+    redirect_to movies_path
+  end
 
-   def index
-     @movies = Movie.filter_by_category(params)
-     @seen_movies = @movies.select {|movie| movie.status == 0 }
-     @unseen_movies = @movies.select {|movie| movie.status == 1 }
-   end
+  def index
+    @movies = Movie.filter_by_category(params)
+    @seen_movies = @movies.select { |movie| movie.status == 0 }
+    @unseen_movies = @movies.select { |movie| movie.status == 1 }
+  end
  end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -26,28 +26,24 @@ class Movie < ApplicationRecord
   def self.get_nominees
     ## this method needs some love
     url = 'http://www.thewrap.com/oscar-nominations-2017-complete-list/'
-    doc =  Nokogiri::HTML(open(url))
+    doc = Nokogiri::HTML(open(url))
     slice = doc.css('p')[11..66]
     (0..slice.length - 1).step(2).each do |index|
-      name = slice[index].text.split("Best")
+      name = slice[index].text.split('Best')
       name = name[1].strip if name.length > 1
-        movies = slice[index+1].text.split(/\r?\n/)
-        movies = movies.map { |line| line.gsub(/[[:punct:]]/, '.').split(".")[3] }
-        binding.pry
-        cat = OscarCategory.create(name: name ) if !OscarCategory.find_by(name: name)
-        cat.add_movies_to_category(movies) if cat
-        binding.pry
+      movies = slice[index + 1].text.split(/\r?\n/)
+      movies = movies.map { |line| line.gsub(/[[:punct:]]/, '.').split('.')[3] }
+      cat = OscarCategory.create(name: name) unless OscarCategory.find_by(name: name)
+      cat.add_movies_to_category(movies) if cat
     end
-
   end
 
   def self.filter_by_category(params)
     if params['name'].nil? || params['name']['category'] == 'All'
-       Movie.all.includes(:oscar_categories, :theaters).order(:title)
+      Movie.all.includes(:oscar_categories, :theaters).order(:title)
     else
       category = params['name']['category']
       OscarCategory.where(name: category).includes(:movies)[0].movies
     end
   end
-  
 end
